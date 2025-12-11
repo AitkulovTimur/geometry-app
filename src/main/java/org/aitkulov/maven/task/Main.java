edbcb9dec3c992c2db21e12ab14aa036aca0c2c6
timuraitkulov <taitkulov@phoenixit.ru>
2025-12-12 01:48:55 +0700

    feat[step 4.3]: add three-dimensional shapes support + rename

--- src/main/java/org/aitkulov/maven/task/Main.java
+++ src/main/java/org/aitkulov/maven/task/Main.java
@@ -3,12 +3,24 @@ package org.aitkulov.maven.task;
 import org.aitkulov.maven.task.manipulation.GeometricQuantities;
 import org.aitkulov.maven.task.manipulation.ShapeUtils;
 import org.aitkulov.maven.task.shape.Circle;
+import org.aitkulov.maven.task.shape.Cube;
 import org.aitkulov.maven.task.shape.Rectangle;
-import org.aitkulov.maven.task.shape.Shape;
+import org.aitkulov.maven.task.shape.RectangularPrism;
+import org.aitkulov.maven.task.shape.Sphere;
+import org.aitkulov.maven.task.shape.ThreeDShape;
 import org.aitkulov.maven.task.shape.Triangle;
+import org.aitkulov.maven.task.shape.TriangularPrism;
+import org.aitkulov.maven.task.shape.TwoDShape;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("------------------------------two dimensional shapes test------------------------------");
+        testTwoDimensionalShape();
+        System.out.println("------------------------------three dimensional shapes test------------------------------");
+        testThreeDimensionalShape();
+    }
+
+    private static void testTwoDimensionalShape() {
         Circle circle = new Circle(5);
         printAreaAndPerimeterInfo(Circle.class.getSimpleName(), circle);
 
@@ -22,11 +34,34 @@ public class Main {
         String resultOfComparing2 = ShapeUtils.compareByQuantityAndGetInfo(rectangle, circle, GeometricQuantities.PERIMETER);
 
         System.out.println(resultOfComparing1);
+        System.out.println("\n");
         System.out.println(resultOfComparing2);
+        System.out.println("\n");
     }
 
-    private static void printAreaAndPerimeterInfo(String shapeName, Shape shapeToProcess) {
+    private static void printAreaAndPerimeterInfo(String shapeName, TwoDShape shapeToProcess) {
         System.out.println("Area of " + shapeName + " is " + shapeToProcess.calculateArea());
         System.out.println("Perimeter of " + shapeName + " is " + shapeToProcess.calculatePerimeter() + "\n");
+    }
+
+    private static void testThreeDimensionalShape() {
+        Sphere sphere = new Sphere(5);
+        printVolumeAndSurfaceArea(Sphere.class.getSimpleName(), sphere);
+
+        Cube cube = new Cube(5);
+        printVolumeAndSurfaceArea(Cube.class.getSimpleName(), cube);
+
+        RectangularPrism cylinder = new RectangularPrism(10, 15, 20);
+        printVolumeAndSurfaceArea(RectangularPrism.class.getSimpleName(), cylinder);
+
+        Triangle triangle = new Triangle(5, 12, 15);
+        TriangularPrism pyramid = new TriangularPrism(triangle, 10);
+        printVolumeAndSurfaceArea(Triangle.class.getSimpleName(), pyramid);
+
+    }
+
+    private static void printVolumeAndSurfaceArea(String shapeName, ThreeDShape shapeToProcess) {
+        System.out.println("Volume of " + shapeName + " is " + shapeToProcess.calculateVolume());
+        System.out.println("Surface area of " + shapeName + " is " + shapeToProcess.calculateSurfaceArea() + "\n");
     }
 }
